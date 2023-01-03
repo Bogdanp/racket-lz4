5012e8be21a0ee45c6e1fb10a53de11b0011cd47
Bogdan Popa <bogdan@defn.io>
2023-01-03 10:03:42 +0200

    xxhash: use wraparound procs to implement uint32{+,-,*}
    Thanks to Matthew Flatt for the suggestion.

--- xxhash-lib/private/xxhash32/unsafe-math.rkt
+++ xxhash-lib/private/xxhash32/unsafe-math.rkt
@@ -14,9 +14,6 @@
 
 (provide (all-defined-out))
 
-(define u32max #xFFFFFFFF)
-(define u32max+1 (add1 u32max))
-
 (begin-encourage-inline
   (define (uint32 bs pos)
     (fxior
@@ -26,35 +23,13 @@
      (fxlshift (unsafe-bytes-ref bs (fx+ pos 3)) 24)))
 
   (define (uint32+ a b)
-    (define v
-      (fx+ a b))
-    (if (fx> v u32max)
-        (fx- v u32max 1)
-        v))
+    (fxand (fx+/wraparound a b) #xFFFFFFFF))
 
   (define (uint32- a b)
-    (define v
-      (fx- a b))
-    (if (fx< v 0)
-        (fx+ u32max+1 v)
-        v))
+    (fxand (fx-/wraparound a b) #xFFFFFFFF))
 
   (define (uint32* a b)
-    (cond
-      [(fx= b 0) 0]
-      [(fx= b 1) a]
-      [(fx> a (fxquotient u32max b))
-       (define a0 (fxand a #xFFFF))
-       (define a1 (fxrshift a 16))
-       (define b0 (fxand b #xFFFF))
-       (define b1 (fxrshift b 16))
-       (define m0 (fx* a0 b0))
-       (define m1 (fx+ (fx* a1 b0) (fxrshift m0 16)))
-       (define mc (fx+ (fx* a0 b1) (fxand m1 #xFFFF)))
-       (define r0 (fx+ (fxlshift mc 16) (fxand m0 #xFFFF)))
-       (fxremainder r0 u32max+1)]
-      [else
-       (fx* a b)]))
+    (fxand (fx*/wraparound a b) #xFFFFFFFF))
 
   (define (uint32rotl a b)
     (define d (fx- 32 b))
